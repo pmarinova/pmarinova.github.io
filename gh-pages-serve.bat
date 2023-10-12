3b6fdd4deb939bfcb082b7155397ef5e423430ab
Plamena Marinova <plamena.marinova@gmail.com>
2023-10-12 11:40:10 +0300

    enable site auto-regeneration on windows

--- gh-pages-serve.bat
+++ gh-pages-serve.bat
@@ -2,4 +2,4 @@ docker run --rm -it ^
 -p 4000:4000 ^
 -v "%cd%:/src/site" ^
 gh-pages ^
-sh -c "bundle install && jekyll serve -H 0.0.0.0 -P 4000" 
+sh -c "bundle install && jekyll serve -H 0.0.0.0 -P 4000 --force_polling" 
